690426bcf6a7794e0a9e79218f08b532de19e92d
b0n3l3ss <b0n3l3ss@github.com>
2019-05-10 15:16:21 -0700

    add actor & ice class w/ basics

--- Actor.h
+++ Actor.h
@@ -2,7 +2,28 @@
 #define ACTOR_H_
 
 #include "GraphObject.h"
-
 // Students:  Add code to this file, Actor.cpp, StudentWorld.h, and StudentWorld.cpp
+
+
+class Actor : public GraphObject {
+private:
+	int imageID;
+	int startX;
+	int startY;
+	Direction startDirection;
+	float size;
+	unsigned int depth;
+public:
+	 Actor (int imID, int strtX, int strtY, Direction strt, float s, unsigned int dpth);
+};
+
+// I got advice, should start w/ ice
+// Should have different parent class
+class Ice : public Actor {
+private: 
+
+public:
+
+};
 
 #endif // ACTOR_H_
