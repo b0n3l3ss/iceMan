49b3d57734c37ec2ab9a265a87be81aab8929071
greg-clark <gregclark03@gmail.com>
2019-05-13 16:31:23 -0700

    Unique to array of pointer

--- StudentWorld.cpp
+++ StudentWorld.cpp
@@ -9,16 +9,17 @@ GameWorld* createStudentWorld(string assetDir)
 // Students:  Add code to this file (if you wish), StudentWorld.h, Actor.h and Actor.cpp
 
 int StudentWorld::init() {
-	// Printed out ice with 2d array
-//	Ice* temp[64][60];
-//	for (int i = 0; i < 64; ++i) {
-//		for (int j = 0; j < 60; ++j){
-//			temp[i][j] = new Ice(i, j);
-//		}
-//	}
+	//Printed out ice with 2d array
+	Ice* temp[64][60];
+	for (int i = 0; i < 64; ++i) {
+		for (int j = 0; j < 60; ++j){
+			temp[i][j] = new Ice(i, j);
+		}
+	}
 	
 	// Unique pointer not pringing out ice
-	unique_ptr<Ice> temp {make_unique<Ice>(0,0)};
+//	unique_ptr<Ice> temp {make_unique<Ice>(0,0)};
+//	auto temp2 = make_unique<Ice>(30,30);
 //	vector<unique_ptr<Ice>> iceVec;
 //	for (int i = 0; i < 64; ++i) {
 //		for (int j = 0; j < 60; ++j){
